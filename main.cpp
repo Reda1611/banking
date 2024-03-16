3e5ae9320755cd3e1aeb0586b1b10512ab3d8086
Reda1611 <134454367+Reda1611@users.noreply.github.com>
2024-03-16 14:40:27 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 #include "services.hpp"
 
 int main() {
-    // Création de la banque
+
     std::unique_ptr<Bank> bank = make_bank();
 
-    // Création d'un client
+
     std::shared_ptr<Client> client = std::make_shared<Client>("John", "password", bank.get());
 
-    // Création d'un compte pour le client
+
     bank->createAccount(client);
 
-    // Autres opérations bancaires...
+
     
     return 0;
 }
