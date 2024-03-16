ba54a3ccb016880bd1d3965ab15f6931b6775fa9
Reda1611 <134454367+Reda1611@users.noreply.github.com>
2024-03-16 14:24:08 +0100

    withdrawal.hpp

--- withdrawal.hpp
+++ withdrawal.hpp
@@ -4,6 +4,7 @@ class Card {
 public:
     virtual int maxWithdrawal() const = 0;
     virtual int maxCredit() const = 0;
+    virtual ~Card() {}
 };
 
 class GoldCard : public Card {
@@ -30,6 +31,43 @@ private:
     CardType& creditCard;
 
 public:
-    Withdrawal(CardType& card);
+    Withdrawal(CardType& card) : creditCard(card) {} 
     int operator()(int amount);
 };
+
+
+
+int GoldCard::maxWithdrawal() const {
+    return 1000; 
+}
+
+int GoldCard::maxCredit() const {
+    return 5000; 
+}
+
+int PlatinumCard::maxWithdrawal() const {
+    
+    return 2000; 
+}
+
+int PlatinumCard::maxCredit() const {
+    
+    return 10000; 
+}
+
+int BlackCard::maxWithdrawal() const {
+    
+    return 5000; 
+}
+
+int BlackCard::maxCredit() const {
+
+    return 20000; 
+}
+
+template<typename CardType>
+int Withdrawal<CardType>::operator()(int amount) {
+
+
+        return amount; 
+}
