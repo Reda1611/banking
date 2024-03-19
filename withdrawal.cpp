d94f1d0dd1693912f9bd01ceb2471be5a533f63d
Reda1611 <134454367+Reda1611@users.noreply.github.com>
2024-03-19 14:23:18 +0100

    withdrawal.cpp

--- withdrawal.cpp
+++ withdrawal.cpp
@@ -1,1 +1,18 @@
 #include "withdrawal.hpp"
+
+using namespace std;
+
+template<typename CardType>
+void Withdrawal<CardType>::operator()(int amount) const {
+    if (amount <= creditCard.maxWithdrawal()) {
+
+      
+        cout << "Retrait de " << amount << " euros effectué avec succès." << endl;
+    } else {
+        cout << "Montant de retrait supérieur au maximum autorisé pour cette carte." << endl;
+    }
+}
+
+template class Withdrawal<GoldCard>;
+template class Withdrawal<PlatinumCard>;
+template class Withdrawal<BlackCard>;
