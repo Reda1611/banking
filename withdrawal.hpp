3f3ab287d725b12238235ba5e9bafb5eb376ba91
Reda1611 <134454367+Reda1611@users.noreply.github.com>
2024-03-19 14:32:51 +0100

    withdrawal.hpp

--- withdrawal.hpp
+++ withdrawal.hpp
@@ -1,37 +1,35 @@
 #pragma once
 
-// Interface abstraite pour les cartes
+
 class Card {
 public:
     virtual int maxWithdrawal() const = 0;
     virtual ~Card() {}
 };
 
-// Exemple de carte : Carte Gold
+
 class GoldCard : public Card {
 public:
     int maxWithdrawal() const override {
-        return 1000; // Exemple : montant maximum de retrait pour la carte Gold
+        return 1000; 
     }
 };
 
-// Exemple de carte : Carte Platinium
+
 class PlatinumCard : public Card {
 public:
     int maxWithdrawal() const override {
-        return 2000; // Exemple : montant maximum de retrait pour la carte Platinium
+        return 2000; 
     }
 };
 
-// Exemple de carte : Carte Black
 class BlackCard : public Card {
 public:
     int maxWithdrawal() const override {
-        return 5000; // Exemple : montant maximum de retrait pour la carte Black
+        return 5000; 
     }
 };
 
-// Template générique pour le retrait
 template<typename CardType>
 class Withdrawal {
 private:
@@ -40,11 +38,10 @@ private:
 public:
     Withdrawal(const CardType& card) : creditCard(card) {} 
 
-    // Opérateur () surchargé pour effectuer le retrait
-    void operator()(int amount) const {
+
+void operator()(int amount) const {
         if (amount <= creditCard.maxWithdrawal()) {
-            // Logique de retrait ici
-            // Pour cet exemple, nous n'implémentons que l'affichage du montant retiré
+            
             std::cout << "Retrait de " << amount << " euros effectué avec succès." << std::endl;
         } else {
             std::cout << "Montant de retrait supérieur au maximum autorisé pour cette carte." << std::endl;
