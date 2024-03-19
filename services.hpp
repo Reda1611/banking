386ce57eff43e05711ac8bfd0086408122e98aac
Reda1611 <134454367+Reda1611@users.noreply.github.com>
2024-03-19 14:03:48 +0100

    services.hpp

--- services.hpp
+++ services.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <memory>
+#include "models.hpp" // Ajout de l'inclusion du fichier models.hpp
 
-class Bank;
+// Fonction pour créer une instance de la classe Bank et la retourner comme un unique_ptr
+std::unique_ptr<Bank> make_bank() {
+    return std::make_unique<Bank>();
+}
 
-std::unique_ptr<Bank> make_bank();
