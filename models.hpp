f8c593e86ece274db2b7f8d23966e059bb831f1a
Reda1611 <134454367+Reda1611@users.noreply.github.com>
2024-03-19 14:04:19 +0100

    models.hpp

--- models.hpp
+++ models.hpp
@@ -2,8 +2,8 @@
 
 #include <string>
 #include <memory>
+#include "withdrawal.hpp" // Ajout de l'inclusion du fichier withdrawal.hpp
 
-class Client;
 class Advisor;
 class Card;
 
@@ -11,7 +11,18 @@ class Bank {
 public:
     Bank();
     void createAccount(std::shared_ptr<Client> client);
-    void assignCard(std::shared_ptr<Client> client, std::unique_ptr<Card> card); 
+    void assignCard(std::shared_ptr<Client> client, std::unique_ptr<Card> card);
+
+    // Fonction pour effectuer un retrait
+    template<typename CardType>
+    void withdraw(std::shared_ptr<Client> client, int amount) {
+        if (client && client->getAccount()) {
+            Withdrawal<CardType> withdrawal(*(client->getAccount()->getCard()), amount);
+            withdrawal(); // Effectue le retrait en utilisant le template Withdrawal
+        } else {
+            std::cout << "Client non valide ou pas de compte associé." << std::endl;
+        }
+    }
 };
 
 class Account {
@@ -23,6 +34,7 @@ private:
 
 public:
     Account(Bank* b, std::shared_ptr<Client> c, std::unique_ptr<Card> crd, double amnt);
+    std::unique_ptr<Card>& getCard(); // Ajout de la méthode pour récupérer la carte
 };
 
 class User {
@@ -50,10 +62,12 @@ private:
     Bank* bank; 
     std::shared_ptr<Advisor> advisor; 
     double cash;
+    std::shared_ptr<Account> account; // Ajout du compte associé au client
 
 public:
-    Client(const std::string& uname, const std::string& pwd, Bank* b, std::shared_ptr<Advisor> adv = nullptr); 
-    void assignAdvisor(std::shared_ptr<Advisor> adv); 
+    Client(const std::string& uname, const std::string& pwd, Bank* b, std::shared_ptr<Advisor> adv = nullptr);
+    void assignAdvisor(std::shared_ptr<Advisor> adv);
+    std::shared_ptr<Account>& getAccount(); // Ajout de la méthode pour récupérer le compte
 };
 
 class ATM {
